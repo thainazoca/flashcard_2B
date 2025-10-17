7979c00d21ee3a24551fb3171b1ccb3eaff3ed60
thainazoca <thaina.barbosa.santos@escola.pr.gov.br>
2025-10-17 08:11:49 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -8,3 +8,27 @@ criaCartao(
     'Qual o maior planeta do sistema solar'
 'Jupiter'
 )
+
+criaCartao(
+    'Biologia'
+    'Qual o maior orgao do corpo humano?'
+    'A pele'
+)
+
+criaCartao(
+    'Ciencias'
+    'Qual o maior pais do mundo?'
+    'Russia'
+)
+
+criaCartao(
+    'Portugues'
+    'Qual o antonimo da palavra "otimismo"?'
+    'Pessimismo'
+)
+
+criaCartao(
+    'Arte'
+    'Em qual museu esta a pintura Mona Lisa'
+    'Museu do Louvre'
+)

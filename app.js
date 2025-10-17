bf101e9744feec682389fb054cfb5d0f3bde23e8
thainazoca <thaina.barbosa.santos@escola.pr.gov.br>
2025-10-17 08:30:01 -0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementBlyd('container');
+    let cartao = document.createElement('article');
+    cartao.ClassName = 'cartao';
+}
 
+cartao.innerHTML =
+<div class="cartao_conteudo">
+    <h3>Programação</h3>
+    <div class="cartao_conteudo_pergunta">
+        <p>O que é JavaScript</p>
+    </div>
+    <div class="cartao_conteudo_resposta">
+        <p>O JavaScript é uma linguagem de Programação</p>
+    </div>
+</div>
